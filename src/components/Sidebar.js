ec2436a40ae845dc52ff4caab57465e1601248fb
sandhya2304 <sandhyasharma0423@gmail.com>
2020-11-06 02:08:48 +0530

    sidebar css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,55 @@
 import React from 'react';
-import { FaTwitter ,FaHome } from "react-icons/fa";
+import { FaTwitter ,
+        FaHome,
+        FaHashtag,
+        FaRegBell,
+        FaRegEnvelope,
+        FaRegBookmark ,
+        FaClipboardList,
+        FaUserAlt,
+        FaMehBlank
+    } from "react-icons/fa";
 
 const Sidebar = () =>{
    return(
        <div className="sidebar">
            <ul>
-               <li><a href=""><FaTwitter/></a></li>
-               <li><a href=""><FaHome/>Home</a></li>
+               <li>
+                   <a href="">
+                       <FaTwitter className="icons logo"/>
+                    </a>
+                </li>
+               <li>
+                   <a href="">
+                       <FaHome className="icons logo"/>Home</a>
+               </li>
+               <li>
+                   <a href="">
+                       <FaHashtag className="icons"/>Explore</a>
+               </li>
+               <li>
+                   <a href="">
+                       <FaRegBell className="icons"/>Notifications</a>
+               </li>
+               <li>
+                   <a href=""><FaRegEnvelope className="icons"/>Messages</a>
+               </li>
+               <li>
+                   <a href=""><FaRegBookmark className="icons"/>Bookmark</a>
+               </li>
+               <li>
+                   <a href=""><FaClipboardList className="icons"/>Lists</a>
+               </li>
+               <li>
+                   <a href=""><FaUserAlt className="icons"/>Profile</a>
+               </li>
+               <li>
+                   <a href=""><FaMehBlank className="icons"/>More</a>
+               </li>
+               <div className="sidebar_Btn">
+                    <a href="">Profile</a>
+
+               </div>
            </ul>
        </div>
    )
